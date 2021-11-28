2025c5ae0f82481bfc9186973bc6cdfd8cdbf1d6
Jake Mills <jakelmills@protonmail.com>
2021-11-28 10:33:53 +0000

    added object for different slides in JS with copy and colors, set these to change with next and prev page button

--- pages.js
+++ pages.js
@@ -3,12 +3,20 @@ let pageNumber = 0
 const nextTag = document.querySelector("footer img.next")
 const prevTag = document.querySelector("footer img.prev")
 const outputTag = document.querySelector("h2")
+const circleTag = document.querySelector("section div.circle")
+const bodyTag = document.querySelector("body")
+const pages = [
+  { copy: "a Brooklyn-based graphic designer", background: "#edc7a9", cirlce: "#3e78ed"},
+  { copy: "Kanye West's biggest fan", background: "#a1fffe", cirlce: "#e34a47"},
+  { copy: "looking for a job at the start of October", background: "#d3c7f3", cirlce: "#f7fe00"},
+  { copy: "letting you <a href='pdf.pdf'>download her PDF</a>", background: "#faffb8", cirlce: "#b472e6"}
+]
 
 const next = function(){
   pageNumber ++
 
-  if (pageNumber > 4) {
-    pageNumber = 1
+  if (pageNumber > pages.length - 1) {
+    pageNumber = 0
   }
 
   updateSection()
@@ -17,15 +25,17 @@ const next = function(){
 const prev = function(){
   pageNumber --
 
-  if (pageNumber < 1) {
-    pageNumber = 4
+  if (pageNumber < 0) {
+    pageNumber = pages.length - 1
   }
 
   updateSection()
 }
 
 const updateSection = function(){
-  outputTag.innerHTML = pageNumber
+  outputTag.innerHTML = pages[pageNumber].copy
+  circleTag.style.backgroundColor = pages[pageNumber].cirlce
+  bodyTag.style.backgroundColor = pages[pageNumber].background
 }
 
 nextTag.addEventListener("click", function(){
